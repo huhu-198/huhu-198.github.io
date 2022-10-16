916fdb64092fd0dd03e85c6fb506de6f23f9015f
huhu <1985989625@qq.com>
2022-10-16 18:07:56 +0800

    静态托管问题

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
 	transpileDependencies: true,
-	publicPath: './',
+	publicPath: '/dist/', //静态托管用，否则是  ./
 });
